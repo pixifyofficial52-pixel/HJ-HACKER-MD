b5f7bfe41907d8cab358619e4a3aee3cadbe3dca
pixifyofficial52-pixel <pixifyofficial52@gmail.com>
2026-09-17 11:31:24 +0500

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,6 @@
+module.exports = {
+    giphyApiKey: process.env.GIPHY_API_KEY || 'dc6zaTOxFJmzC',
+    ownerNumber: process.env.OWNER_NUMBER || '923266571331',
+    botName: 'HJ-HACKER MD',
+    ownerName: 'HJ-HACKER'
+};
