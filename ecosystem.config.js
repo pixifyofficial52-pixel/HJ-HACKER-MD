a39c8101e80be618bc09bb9387ee3557e75bb216
pixifyofficial52-pixel <pixifyofficial52@gmail.com>
2026-09-17 11:06:03 +0500

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,1 +1,14 @@
+module.exports = {
+  apps : [{
+    name: "hj-hacker-bot",
+    script: "./index.js",
+    watch: false,
+    autorestart: true,
+    max_memory_restart: '1G',
+    node_args: '--max-old-space-size=1024',
+    env: {
+      NODE_ENV: "production",
+    }
+  }]
+};
 
